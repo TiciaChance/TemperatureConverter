40fd1e6094c430a849e1f87dd1d53f3132b0146c
TiciaChance <laticia.chance@flatironschool.com>
2016-11-27 00:05:50 -0500

    delegates and methods for uipickerview

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
 
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var tempPicker: UIPickerView!
+    @IBOutlet var tempRange: TemperatureRange!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
